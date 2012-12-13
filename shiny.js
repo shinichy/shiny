37a719c052324994ed46d11583c5f46169d77f38
Shinichi Katayama <shin1.katayama@gmail.com>
2012-12-12 20:42:15 -0800

    Make sidebar resizable

--- shiny.js
+++ shiny.js
@@ -2,11 +2,9 @@ var gui = require('nw.gui');
 var win = gui.Window.get();
 
 onload = function() {
-	console.log(localStorage.width)
 	if (localStorage.width && localStorage.height) {
 		win.resizeTo(Number(localStorage.width), Number(localStorage.height));
 		win.moveTo(Number(localStorage.x), Number(localStorage.y));
-		console.log("window resized")
 	}
 	win.show();
 }
@@ -17,7 +15,6 @@ win.on('close', function() {
 	localStorage.y      = win.y;
 	localStorage.width  = win.width;
 	localStorage.height = win.height;
-	console.log("save window pos")
 	this.close(true);
 });
 
@@ -38,3 +35,7 @@ editMenu.append(new gui.MenuItem({label: "Undo", click: function () {
 	alert('Undo')
 }}))
 win.menu = menubar
+
+$( "#sidebar" ).resizable({
+	handles: 'e'
+});
