d7548b4e7ae77cf373d26be3a7dfd473d9ac84b2
Shinichi Katayama <shin1.katayama@gmail.com>
2012-12-17 19:22:57 -0800

    Auto detect charset

--- shiny.js
+++ shiny.js
@@ -1,4 +1,7 @@
 var gui = require('nw.gui');
+var fs = require('fs')
+var jschardet = require('jschardet')
+
 var win = gui.Window.get();
 
 onload = function() {
@@ -18,6 +21,7 @@ win.on('close', function() {
 	this.close(true);
 });
 
+// init Ace
 var editor = ace.edit("editor");
 editor.setTheme("ace/theme/twilight");
 editor.getSession().setMode("ace/mode/javascript");
@@ -26,9 +30,10 @@ editor.getSession().setMode("ace/mode/javascript");
 var menubar = new gui.Menu({type: "menubar"});
 var fileMenu = new gui.Menu()
 menubar.append(new gui.MenuItem({label: "File", submenu: fileMenu}))
-fileMenu.append(new gui.MenuItem({label: "New", click: function () {
-	alert('New')
+fileMenu.append(new gui.MenuItem({label: "Open...", click: function () {
+	chooseFile('#fileDialog')
 }}))
+
 var editMenu = new gui.Menu()
 menubar.append(new gui.MenuItem({label: "Edit", submenu: editMenu}))
 editMenu.append(new gui.MenuItem({label: "Undo", click: function () {
@@ -39,3 +44,26 @@ win.menu = menubar
 $( "#sidebar" ).resizable({
 	handles: 'e'
 });
+
+function chooseFile(name) {
+	var chooser = $(name);
+	chooser.trigger('click');
+	chooser.change(function(evt) {
+		console.log($(this).val());
+		fs.readFile($(this).val(), function (err, buf) {
+			if (err) {
+				console.log(err)
+				return
+			}
+			var encoding = jschardet.detect(buf).encoding
+			if (encoding === 'SHIFT_JIS') {encoding = 'SJIS'}
+			else if (encoding === 'EUC-JP') {encoding = 'EUCJP'}
+			else if (encoding === 'ISO-2022-JP') {encoding = 'JIS'}
+			console.log(encoding)
+
+			var utf8Array = Encoding.convert(buf, 'UNICODE', encoding)
+			var content = Encoding.codeToString(utf8Array)
+			editor.setValue(content)
+		})
+	});
+}
