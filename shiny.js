15895c187811495647c1269a657f40518620e5a4
Shinichi Katayama <shin1.katayama@gmail.com>
2012-12-26 23:23:32 -0800

    Fix #3 File Dialog's change event is called multiple times.

--- shiny.js
+++ shiny.js
@@ -24,40 +24,38 @@ function _onReady () {
 	editor.setTheme("ace/theme/twilight");
 	editor.getSession().setMode("ace/mode/javascript");
 
-	function _chooseFile(name) {
-		var chooser = $(name);
-		chooser.trigger('click');
-		chooser.change(function(evt) {
-			var path = $(this).val();
-			console.log(path);
-			fs.readFile(path, function (err, buf) {
-				if (err) {
-					console.log(err);
-					return;
-				}
-				var encoding = jschardet.detect(buf).encoding;
-				if (encoding === 'SHIFT_JIS') {encoding = 'SJIS'}
-				else if (encoding === 'EUC-JP') {encoding = 'EUCJP'}
-				else if (encoding === 'ISO-2022-JP') {encoding = 'JIS'}
-				console.log(encoding);
+	// init file dialog
+	var fileDialog = $('#fileDialog');
+	fileDialog.change(function(evt) {
+		var path = $(this).val();
+		console.log(path);
+		fs.readFile(path, function (err, buf) {
+			if (err) {
+				console.log(err);
+				return;
+			}
+			var encoding = jschardet.detect(buf).encoding;
+			if (encoding === 'SHIFT_JIS') {encoding = 'SJIS'}
+			else if (encoding === 'EUC-JP') {encoding = 'EUCJP'}
+			else if (encoding === 'ISO-2022-JP') {encoding = 'JIS'}
+			console.log(encoding);
 
-				var utf8Array = Encoding.convert(buf, 'UNICODE', encoding);
-				var content = Encoding.codeToString(utf8Array);
-				var file = new File(path, content);
-				editor.getSession();
-				editor.setValue(file.getDocument().getValue());
-				// below code is so slow.
-				// editor.getSession().setDocument(file.getDocument());
-			})
-		});
-	}
+			var utf8Array = Encoding.convert(buf, 'UNICODE', encoding);
+			var content = Encoding.codeToString(utf8Array);
+			var file = new File(path, content);
+			editor.getSession();
+			editor.setValue(file.getDocument().getValue());
+			// below code is so slow.
+			// editor.getSession().setDocument(file.getDocument());
+		})
+	});	
 
 	// create menus
 	var menubar = new gui.Menu({type: "menubar"});
 	var fileMenu = new gui.Menu()
 	menubar.append(new gui.MenuItem({label: "File", submenu: fileMenu}))
 	fileMenu.append(new gui.MenuItem({label: "Open...", click: function () {
-		_chooseFile('#fileDialog')
+		$('#fileDialog').trigger('click');
 	}}))
 
 	var editMenu = new gui.Menu()
