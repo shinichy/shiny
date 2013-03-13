4a621d94a301d8759bd907f20e52e3d3167c93cd
Shinichi Katayama <shin1.katayama@gmail.com>
2013-03-12 20:50:38 -0700

    Update grunt version to 0.4. Modify to use grunt-contrib-jasmine.

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+/*jshint indent:2 */
+/*global module, require */
+
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    connect: {
+      port : 8000
+    },
+    jasmine: {
+      tests: {
+        options: {
+          specs: 'test/spec/**/*Spec.js',
+          vendor: [
+          'src/lib/jquery-1.8.3.min.js',
+          'src/lib/underscore-min.js',
+          'src/lib/backbone-min.js',
+          'src/lib/bootstrap.js'
+          ],
+          host: 'http://127.0.0.1:<%= connect.port %>/',
+          template: require('grunt-template-jasmine-requirejs'),
+          templateOptions: {
+            requireConfig: {
+              baseUrl: 'src/',
+              paths: {
+                ace: 'lib/ace'
+              }
+            }
+          }
+        }
+
+      }
+    },
+    requirejs: {
+      compile: {
+        options: {
+          appDir: './src',
+          baseUrl: './',
+          mainConfigFile: './src/shiny.js',
+          dir: './build',
+          modules: [
+          {
+            name: 'shiny'
+          }
+          ]
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.registerTask('default', ['connect', 'jasmine']);
+  grunt.registerTask('build', ['requirejs']);
+};
